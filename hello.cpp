aa9c25e0dd587e3d46b219d960e8b1896bd78395
NealGragg <ngragg@uncc.edu>
2021-02-04 20:23:26 -0500

    activity programming done

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,19 @@
-int main () {
+#include <iostream>
+#include <unistd.h>
+#include <sys/socket.h>
 
+using namespace std;
+
+int main (int argc, char **argv)
+{
+  int hostInt;
+  char hostName[50];
+
+  hostInt = gethostname(hostName, sizeof(hostName));
+
+  if(hostInt == 0)
+    {
+      std::cout<<"The host machine is "<< hostName <<endl;
+    }
   return 0;
 }
